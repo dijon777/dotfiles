f1da8093b9cda34cc58d706108d4cf1bcfe16616
Taka Matsumoto <dijon777@gmail.com>
2018-03-12 23:52:17 +0900

    awsへのsyncスクリプト

--- scripts/sync-aws.sh
+++ scripts/sync-aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 store_dir=/var/research/backup/research.awsvr.com/enqdb
 
-#s3へ保存
-aws s3 sync $store_dir s3://wine-research/mysql/ --dryrun
+#s3へ保存 (exact-timestampsがないとファイルサイズが同じだと同期されない)
+aws s3 sync --exact-timestamps --delete $store_dir s3://wine-research/mysql/
